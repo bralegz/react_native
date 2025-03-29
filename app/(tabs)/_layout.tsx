fdfbb5b43c99463d8ed3c1f3ecda0542967186cb
bralegz <bralegz@gmail.com>
2025-03-29 14:40:15 -0500

    basic tab navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,15 +5,36 @@ import { Tabs } from 'expo-router'
 const _Layout = () => {
   return (
     <Tabs>
-      <Tabs.Screen 
+      <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
           headerShown: false
         }}
       />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: 'Search',
+          headerShown: false
+        }}
+      />
+      <Tabs.Screen
+        name="saved"
+        options={{
+          title: 'Saved',
+          headerShown: false
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          headerShown: false
+        }}
+      />
     </Tabs>
-  )
+  );
 }
 
 export default _Layout
